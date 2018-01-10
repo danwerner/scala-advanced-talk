410c554a4cb3452f94eea1113ddbc22f99c1c136
Daniel Werner <daniel.d.werner@gmail.com>
2018-01-10 13:19:52 +0100

    Final talk

--- app/Solution.scala
+++ app/Solution.scala
@@ -1,16 +1,21 @@
+//
+// Scala Advanced Features
+//
+
 import play.api.mvc.{Action, Request, Results}
-import play.mvc.Http.Status
 
-import scala.concurrent.Future
+import scala.concurrent.{Await, ExecutionContext, Future}
 
-object Solution extends Results {
+object ImplicitParametersSolution extends Results {
 
-  // Implicit parameters
+  // 1. Implicit parameters
 
   // Request
   def formatValue(v: String)(implicit request: Request[_]): String = {
-    val formatO = request.headers.get("Accepts")
-    ???
+    request.headers.get("Accepts") match {
+      case Some("text/html") => "<b>" + v + "</b>"
+      case _ => v
+    }
   }
 
   def index = Action { implicit request =>
@@ -20,28 +25,131 @@ object Solution extends Results {
   // Future
   import scala.concurrent.ExecutionContext.Implicits.global
 
+  // implicitly
   Future { 1 + 2 }.map(_ * 2)
 
-  // Implicit conversions
+  // explicitly
+  Future { 1 + 2 }.map(_ * 2)(global)
 
-  // FiniteDuration to java.time.Duration
-  // Syntax: ArrowAssoc to Tuple2
+  implicit val executionContext: ExecutionContext = ExecutionContext.fromExecutor(null)
 
-  // Implicit classes
+  implicit def makeExecutionContext: ExecutionContext = ExecutionContext.fromExecutor(null)
+}
+
+
+object ImplicitConversionsSolution {
+
+  // 2. Implicit conversions
+
+  // java.time.Duration to FiniteDuration
+  import scala.concurrent.duration.{Duration => ScalaDuration, _}
+  import scala.language.postfixOps
+
+  val f = Future { 1 + 2 }
+
+  // Uses Scala Duration
+  Await.result(f, 5 seconds)
+
+  val timeout = java.time.Duration.ofSeconds(5)
+
+  // Uses java.time.Duration, automatically
+  Await.result(f, timeout)
+
+  implicit def durationToScalaDuration(d: java.time.Duration): ScalaDuration = d.getSeconds.seconds
+}
+
+
+object ImplicitClassesSolution {
+
+  // 3. Implicit classes
+
+  implicit class FooString(self: String) {
+    def foo(n: Int) = self + " " + n
+  }
+
+  "bar".foo(433)
 
   // DurationInt, DurationLong
-  // Own example
+  import scala.concurrent.duration.{DurationInt, DurationLong}
+
+  5.seconds
+
+  1 minute
+
+  // Use case: Extend Future with new combinator methods
+
+  // Syntax: ArrowAssoc to Tuple2
+  Map("foo" -> 1, "bar" -> 2)
+}
 
 
-  // Manifests & TypeTags
+object ManifestsTypeTagsSolution {
 
-  // Manifests (shortly, deprecated!)
-  // Type Tags, typeOf[T] -- why to use
+  // 4. Manifests & TypeTags
+
+  // This doesn't work
+  def whatsTheList[T](xs: List[T]) = {
+    if (xs.isInstanceOf[List[String]])
+      println("string list")
+    else
+      println("other list")
+  }
+
+  // Manifests are deprecated
+
+  // Type Tags, typeOf[T]
+  import scala.reflect.runtime.universe._
+
+  def whatsTheList[T: TypeTag](xs: List[T]) = {
+    if (typeOf[T] <:< typeOf[String])
+      println("string list")
+    else
+      println("other list")
+  }
+}
 
 
-  // Scalaz Features
+object ScalazSolution {
+
+  // 5. Scalaz Features
+
+  // Import Fun!!!
+  import scalaz.std.map._
+  import scalaz.std.list._
+  import scalaz.syntax.semigroup._
+  import scalaz.syntax.monoid._
+
+  // Monoids
+  0 + 1
+  Nil ++ List(1, 2)
+  Map.empty ++ Map("x" -> 1)
+
+  Map("foo" -> List(1, 2)) |+| Map("foo" -> List(3), "bar" -> List(4))
+
+  // Another use case: Combine the results of Futures with |+|
 
   // Tagged Types (@@)
-  // Something else?
+  import scalaz.{@@, Tag}
 
+  object Tags {
+    trait UserId
+  }
+
+  type UserId = String @@ Tags.UserId
+
+  def UserId(s: String): UserId = Tag[String, Tags.UserId](s)
+
+  def showUser(userId: UserId) = ???
+
+  val uid = UserId("JFK")
+
+  showUser(uid)
+
+  // Won't work -- String is not compile-time compatible with UserId!
+  //showUser("JKF")
+
+  // Won't work -- can't mess with tagged Strings!
+  //uid.toLowerCase
+
+  Tag.unwrap(uid).toLowerCase
 }
